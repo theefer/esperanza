72052202b02311110f325f7c24e9d568029cb90e
Tobias Rundstrom <tru@xmms.org>
2006-12-01 11:51:02 -0300

    Added dutch translation.

---
+++ esperanza_nl.ts
@@ -0,0 +1,521 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="nl_NL">
+<context>
+    <name>AddServerDialog</name>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="137"/>
+        <source>Adding new server</source>
+        <translation>Voeg een nieuwe server toe</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="140"/>
+        <source>Server name</source>
+        <translation>Servernaam</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="145"/>
+        <source>Server path</source>
+        <translation>Server adres</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="150"/>
+        <source>Add</source>
+        <translation>Voeg toe</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="152"/>
+        <source>Cancel</source>
+        <translation>Annuleer</translation>
+    </message>
+</context>
+<context>
+    <name>BrowseDialog</name>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="75"/>
+        <source>Parent Directory</source>
+        <translation>Bovenliggende map</translation>
+    </message>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="83"/>
+        <source>List View</source>
+        <translation>Lijst</translation>
+    </message>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="91"/>
+        <source>Detail View</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="99"/>
+        <source>Look in:</source>
+        <translation>Locatie:</translation>
+    </message>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="104"/>
+        <source>Open</source>
+        <translation>Open</translation>
+    </message>
+    <message>
+        <location filename="ui/browsedialog.cpp" line="109"/>
+        <source>Cancel</source>
+        <translation>Annuleer</translation>
+    </message>
+</context>
+<context>
+    <name>InfoWindow</name>
+    <message>
+        <location filename="ui/infowindow.cpp" line="41"/>
+        <source>Esperanza - Info</source>
+        <translation>Esperanza - Informatie</translation>
+    </message>
+</context>
+<context>
+    <name>KeyInputDialog</name>
+    <message>
+        <location filename="ui/preferencessupport.cpp" line="60"/>
+        <source>Press key to act as shortcut:</source>
+        <translation>Druk een toets als sneltoest:</translation>
+    </message>
+    <message>
+        <location filename="ui/preferencessupport.cpp" line="94"/>
+        <source>None</source>
+        <translation>Geen</translation>
+    </message>
+    <message>
+        <location filename="ui/preferencessupport.cpp" line="69"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="ui/preferencessupport.cpp" line="70"/>
+        <source>Cancel</source>
+        <translation>Annuleer</translation>
+    </message>
+</context>
+<context>
+    <name>KeyWidget</name>
+    <message>
+        <location filename="ui/preferencessupport.cpp" line="119"/>
+        <source>None</source>
+        <translation>Geen</translation>
+    </message>
+</context>
+<context>
+    <name>LastFmDialog</name>
+    <message>
+        <location filename="ui/lastfm.cpp" line="48"/>
+        <source>Similar artists to:</source>
+        <translation>Artiesten zoals:</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="52"/>
+        <source>None</source>
+        <translation>Geen</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="67"/>
+        <source>No data ...</source>
+        <translation>Geen gegevens ...</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="116"/>
+        <source>Search for in Medialib</source>
+        <translation>Zoek naar in bibliotheek</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="124"/>
+        <source>Open artist at Last.fm</source>
+        <translation>Open artiest op Last.fm</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="127"/>
+        <source>Open artist at Musicbrainz</source>
+        <translation>Open artiest op Musicbrainz</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="131"/>
+        <source>Search for artist on oink</source>
+        <translation>Zoek naar artiest op oink</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="171"/>
+        <source>Error - try again</source>
+        <translation>Fout - Probeer opnieuw</translation>
+    </message>
+    <message>
+        <location filename="ui/lastfm.cpp" line="241"/>
+        <source>Loading data</source>
+        <translation>Gegevens worden geladen</translation>
+    </message>
+</context>
+<context>
+    <name>MedialibDialog</name>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="49"/>
+        <source>All</source>
+        <translation>Alles</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="50"/>
+        <source>Artist</source>
+        <translation>Artiest</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="51"/>
+        <source>Album</source>
+        <translation>Album</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="52"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="53"/>
+        <source>Year</source>
+        <translation>Jaar</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="60"/>
+        <source>=</source>
+        <translation>=</translation>
+    </message>
+    <message>
+        <location filename="ui/medialibdialog.cpp" line="82"/>
+        <source>Display non-available entries</source>
+        <translation>Toon onbeschikbare nummers</translation>
+    </message>
+</context>
+<context>
+    <name>MiniMode</name>
+    <message>
+        <location filename="ui/minimode.cpp" line="34"/>
+        <source>Esperanza</source>
+        <translation>Esperanza</translation>
+    </message>
+</context>
+<context>
+    <name>PlayerWidget</name>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="174"/>
+        <source>&amp;File</source>
+        <translation>&amp;Bestand</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="430"/>
+        <source>Preferences</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="435"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="177"/>
+        <source>Playlist</source>
+        <translation>Speellijst</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="406"/>
+        <source>Add local file</source>
+        <translation>Voeg lokaal bestand toe</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="407"/>
+        <source>Add local dir</source>
+        <translation>Voeg lokale map toe</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="409"/>
+        <source>Add remote file</source>
+        <translation>Voeg remote bestand toe</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="411"/>
+        <source>Add files from medialib</source>
+        <translation>Voeg bestanden uit bibliotheek toe</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="184"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="185"/>
+        <source>Esperanza Help</source>
+        <translation>Esperanza - Help</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="431"/>
+        <source>Last.fm browser</source>
+        <translation>Last.fm browser</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="433"/>
+        <source>Shuffle</source>
+        <translation>Willekeurig afspelen</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="436"/>
+        <source>Keyboard shortcuts</source>
+        <translation>Sneltoetsen</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="527"/>
+        <source>Remove selected files</source>
+        <translation>Verwijder geselecteerde bestanden</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="528"/>
+        <source>Clear playlist</source>
+        <translation>Leeg speellijst</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="591"/>
+        <source>Server died. The application will now quit.</source>
+        <translation>De server is gestopt. Esperanza wordt nu gestopt.</translation>
+    </message>
+    <message>
+        <location filename="ui/playerwidget.cpp" line="642"/>
+        <source>Esperanza is now playing:</source>
+        <translation>Esperanza speelt nu:</translation>
+    </message>
+</context>
+<context>
+    <name>PreferenceDialog</name>
+    <message>
+        <location filename="ui/preferences.cpp" line="100"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="101"/>
+        <source>Value</source>
+        <translation>Waarde</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="116"/>
+        <source>Save</source>
+        <translation>Opslaan</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="120"/>
+        <source>Discard</source>
+        <translation>Annuleer</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="123"/>
+        <source>Reset</source>
+        <translation>Herstel</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="257"/>
+        <source>Reset settings?</source>
+        <translation>Instellingen herstellen?</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="258"/>
+        <source>Are your sure you want to reset program to standard values?</source>
+        <translation>Weet u zeker dat u de standaard instellingen wilt herstellen?</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="259"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="259"/>
+        <source>No</source>
+        <translation>Nee</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="44"/>
+        <source>Autostart xmms2d if not running</source>
+        <translation>Autostart xmms2d wanneer deze niet draait</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="45"/>
+        <source>Number of songs to randomly add</source>
+        <translation>Aantal nummers dat willekeurig wordt toegevoegd</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="46"/>
+        <source>Ignore locale settings and use default language</source>
+        <translation>Negeer taal instellingen en gebruik standaard taal</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="47"/>
+        <source>Show serverbrowser on startup</source>
+        <translation>Toon serverbrowser bij het opstarten</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="48"/>
+        <source>Jump to current entry on song change</source>
+        <translation>Spring naar huidige nummer bij nummerwissel</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="49"/>
+        <source>Use compact (boring) playlist mode</source>
+        <translation>Gebruik compacte (saaie) speellijst modus</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="50"/>
+        <source>Show albumart under artist</source>
+        <translation>Toon album afbeelding onder artiest</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="51"/>
+        <source>Show a stop button as well</source>
+        <translation>Toon ook een stop knop</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="52"/>
+        <source>Use reversed elapsed time</source>
+        <translation>Gebruik aflopende tijd</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="53"/>
+        <source>Size of album art cache in kb</source>
+        <translation>Grootte van album afbeelding cache in kb</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="54"/>
+        <source>Values to be shown in context row (comma-separated)</source>
+        <translation>Te tonen waardes in context rij (komma gescheiden)</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="55"/>
+        <source>Draw the contextarea in a bright color</source>
+        <translation>Toon het contextvak in een felle kleur</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="56"/>
+        <source>Paint the progress in a lighter color</source>
+        <translation>Toon de verloop balk in een lichte kleur</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="57"/>
+        <source>Show volume in a popup</source>
+        <translation>Toon het volume in een popup</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="58"/>
+        <source>Change volume interactivly, could cause problems.</source>
+        <translation>Verander volume interactief, kan problemen veroorzaken.</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="59"/>
+        <source>Show oink search in last.fm context</source>
+        <translation>Toon oink zoeken in last.fm scherm</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="61"/>
+        <source>Show system tray icon</source>
+        <translation>Toon systeem icoon</translation>
+    </message>
+    <message>
+        <location filename="ui/preferences.cpp" line="62"/>
+        <source>Show notification on song change</source>
+        <translation>Toon notificatie bij nummer wissel</translation>
+    </message>
+</context>
+<context>
+    <name>ServerDialog</name>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="40"/>
+        <source>Welcome to an XMMS2 client
+Please select the server to connect to.
+It&apos;s possible to disable this dialog in preferences</source>
+        <translation>Welkom bij een XMMS2 client
+Selecteer de server waarmee verbinding moet worden gemaakt.
+Het is mogelijk om deze dialoog uit te schakelen in de instellingen</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="73"/>
+        <source>connect</source>
+        <translation>verbindt</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="77"/>
+        <source>quit</source>
+        <translation>sluit</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="168"/>
+        <source>Dang! That name is already taken!</source>
+        <translation>Die naam is al in gebruik!</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="186"/>
+        <source>Eeep! Can&apos;t remove mDNS items!</source>
+        <translation>Kan geen mDNS server verwijderen!</translation>
+    </message>
+    <message>
+        <location filename="ui/serverdialog.cpp" line="192"/>
+        <source>Eeep! Can&apos;t remove local item!</source>
+        <translation>Kan geen lokaal item verwijderen!</translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="ui/systemtray.cpp" line="42"/>
+        <source>Nothing</source>
+        <translation>Niets</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="104"/>
+        <source>Hide main window</source>
+        <translation>Verberg hoofdvenster</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="47"/>
+        <source>Play/Pause</source>
+        <translation>Afspelen/Pause</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="51"/>
+        <source>Stop</source>
+        <translation>Stop</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="55"/>
+        <source>Next</source>
+        <translation>Volgende</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="57"/>
+        <source>Previous</source>
+        <translation>Vorige</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="60"/>
+        <source>Exit</source>
+        <translation>Afsluiten</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="83"/>
+        <source>Play</source>
+        <translation>Afspelen</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="86"/>
+        <source>Pause</source>
+        <translation>Pause</translation>
+    </message>
+    <message>
+        <location filename="ui/systemtray.cpp" line="107"/>
+        <source>Show main window</source>
+        <translation>Toon hoofdvenster</translation>
+    </message>
+</context>
+<context>
+    <name>TextDialog</name>
+    <message>
+        <location filename="ui/textdialog.cpp" line="35"/>
+        <source>Close</source>
+        <translation>Afsluiten</translation>
+    </message>
+</context>
+</TS>
